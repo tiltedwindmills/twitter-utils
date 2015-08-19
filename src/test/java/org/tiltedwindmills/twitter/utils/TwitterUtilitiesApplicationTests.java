891ed21593c4d21c92bc02792241f9ad19e462a3
John Daniel <johndaniel@gmail.com>
2015-08-19 15:01:40 -0400

    Added test properties to get Spring over the missing Twitter OAuth creds.

--- src/test/java/org/tiltedwindmills/twitter/utils/TwitterUtilitiesApplicationTests.java
+++ src/test/java/org/tiltedwindmills/twitter/utils/TwitterUtilitiesApplicationTests.java
@@ -2,17 +2,18 @@ package org.tiltedwindmills.twitter.utils;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TwitterUtilitiesApplication.class)
+@TestPropertySource("classpath:test.properties")
 @WebAppConfiguration
 public class TwitterUtilitiesApplicationTests {
 
 	@Test
-	public void contextLoads() {
-	}
+    public void contextLoads() { }
 
 }
